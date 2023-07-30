3439eb2dbca183382ee116af1b8f213322d7cdd7
sungho <sungho8203@gmail.com>
2023-07-30 21:29:36 +0900

    카카오맵 불러오고 현재위치 표시

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-const http = require('http');
+let mapContainer = document.getElementById('MAP'); // 지도를 표시할 div 
 
-const hostname = '0.0.0.0';
-const port = 3000;
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+let map; 
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!!!!\n');
+let curLatitude;
+let curLongitude;
+
+let seqMap = new Promise(function(resolve, reject){
+    navigator.geolocation.getCurrentPosition(function(pos){
+        curLatitude = pos.coords.latitude;
+        curLongitude = pos.coords.longitude;
+        resolve("success load curPos");
+    });
+});
+
+seqMap.then((val) => {
+    console.log(val);
+
+    let mapOption = { 
+        center: new kakao.maps.LatLng(curLatitude, curLongitude), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
+    };
+
+    map = new kakao.maps.Map(mapContainer, mapOption);
+});
+seqMap.then(navigator.geolocation.watchPosition(function (pos){
+    console.log("run watchPosition");
+
+    curLatitude = pos.coords.latitude;
+    curLongitude = pos.coords.longitude;
+
+    displayCurMarker();
+}));
+
+seqMap.catch(() => {
+    console.log ("exception");
 });
 
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
+function displayCurMarker(){
+
+    // 마커가 표시될 위치입니다 
+    let markerPosition  = new kakao.maps.LatLng(curLatitude, curLongitude); 
+
+    let imgSrc = './curPos.png',
+        imgSize = new kakao.maps.Size(30);
+
+    let markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
+
+    // 마커를 생성합니다
+    let marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image : markerImg
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+}
