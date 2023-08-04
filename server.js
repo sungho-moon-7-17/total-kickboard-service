823ae24f30fcbc2adf852c185e802f2c5f18be39
sungho <sungho8203@gmail.com>
2023-08-05 03:15:24 +0900

    mysql 사용

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 
+const pool = mysql.createPool({
+  connectionLimit : 4,
+  host : "127.0.0.1",
+  user : "root",
+  password : "1234",
+  database : "TEST"
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/front/index.html");
 });
 
 app.get("/data", (req, res) => {
-  let response = {
-    j : 10,
-    i : "temp"
-  }
-  res.send(response);
+  pool.query("select * from test", (error, result) => {
+    res.send(result);
 
-  console.log("request temp");
+    console.log(pool);
+    // console.log("request temp");
+  });
 });
 
-app.use(express.static(__dirname));
-
+app.use(express.static(__dirname + "/front"));
 
 app.use((req, res, next) => {
   res.status(404).send("Not found");
