a40e3e444301fd8b118b8585884951659eb5dd4b
sungho <sungho8203@gmail.com>
2023-08-05 03:16:14 +0900

    누락된 파일 추가

---
+++ front/marker.js
@@ -0,0 +1,41 @@
+class Marker{
+    marker;
+
+    latitude;
+    longitude;
+
+    constructor(latitude, longitude, src){
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        let markerPos = new kakao.maps.LatLng(latitude, longitude);
+
+        if (src){
+            let imgSrc = src,
+                imgSize = new kakao.maps.Size(30);
+            let markerImg = new kakao.maps.MarkerImage(imgSrc, imgSize);
+
+            this.marker = new kakao.maps.Marker({
+                position : markerPos,
+                image : markerImg
+            });
+        }
+        else {
+            this.marker = new kakao.maps.Marker({
+                position : markerPos
+            });
+        }
+
+        this.marker.setMap(map);
+    }
+
+    update(latitude, longitude){
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+        let latlng = new kakao.maps.LatLng(latitude, longitude);
+
+        this.marker.setPosition(latlng);
+        console.log(latitude, longitude);
+    }
+}
